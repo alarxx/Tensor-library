4d7a3fdea3d0210bd6e55d77d016034e1df6183b
Alar Akilbekov <alar.akilbekov@gmail.com>
2026-01-07 07:15:09 +0500

    refactor: Tensor creation and access by index mapping, major functionality

--- src/Tensor/Tensor.hpp
+++ src/Tensor/Tensor.hpp
@@ -1,3 +1,7 @@
+#pragma once
+#ifndef TENSOR_HPP
+#define TENSOR_HPP
+
 /*
     SPDX-License-Identifier: MPL-2.0
     --------------------------------
@@ -10,160 +14,455 @@
 
     Provided “as is”, without warranty of any kind.
 
-    Copyright © 2025 Alar Akilbekov. All rights reserved.
+    Copyright © 2026 Alar Akilbekov. All rights reserved.
  */
 
-#pragma once
-#ifndef _TENSOR_H_
-#define _TENSOR_H_
-
-#define DEBUG_TENSOR true
-#define DEBUG_LOG_TENSOR true
+// Макрос для создания вложенных std::initializer_list
+#define INITIALIZER_LIST_1(T) std::initializer_list<T>
+#define INITIALIZER_LIST_2(T) std::initializer_list<INITIALIZER_LIST_1(T)>
+#define INITIALIZER_LIST_3(T) std::initializer_list<INITIALIZER_LIST_2(T)>
+#define INITIALIZER_LIST_4(T) std::initializer_list<INITIALIZER_LIST_3(T)>
 
 #include <iostream>
+#include <cassert>
 #include <stdexcept>
 #include <string>
 #include <type_traits>
 #include <concepts>
-
-#include "utils/log.tpp"
-#include "utils/Arithmetic.tpp"
+#include <initializer_list>
+#include <vector>
 
 namespace tensor {
-/*
-    Multidimensional Array with Mappings
- */
-template <Arithmetic T = double>
+
+template <typename T>
+concept Arithmetic = requires(T a, T b){
+    // requires std::is_arithmetic_v<T>;
+
+    // specific check for what we need
+    {a += b};
+    {a -= b};
+    {a *= b};
+    {a /= b};
+
+    { a + b } -> std::same_as<T>;
+    { a - b } -> std::same_as<T>;
+    { a * b } -> std::same_as<T>;
+    { a / b } -> std::same_as<T>;
+};
+
+
+template <Arithmetic T = float>
 class Tensor {
-protected:
-    T _value;
-    int _rank; // not mathematically correct name, его тоже можно вычислить рекурсивно, оставляю для debug-а
-    // Не получится выводить rank, например так: sizeof(_coeffs) / (_size * sizeof(T))
-    int _size; // better be unsigned int
-    // Tensor<T> * _coeffs;
-    T * _coeffs; // _size = _coeffs.length
-    int * _shape; // _rank = _shape.length
-
+private:
+    T * coeffs;
+    int * dims; // shape
+    int rank; // not mathematically correct name, его тоже можно вычислить рекурсивно, оставляю для debug-а
 public:
-    explicit Tensor() : _value(0), _rank(0), _size(-1), _coeffs(nullptr), _shape(nullptr) {}
+    using type = T;
 
-    explicit Tensor(std::integral auto ... args){
-        log("Basic Constructor");
-
-        int dims[] = {args...}; // arguments expansion
-        int length = sizeof...(args);
-
-        if(length <= 0){ // never less actually
-            throw std::runtime_error("Error: empty dims!");
-        }
-
-        _rank = length;
-        _shape = new int[length]; // dims is in Stack memory
-        for(int i = 0; i < length; i++){
-            _shape[i] = dims[0];
-        }
-
-
-        // this constructor is never scalar, at least vector of size 1 * dims[0]
-        _size = 1;
-        for(int i = 0; i < _rank; i++){
-            _size *= _shape[i];
-        }
-
-        log("_rank: ", _rank, "; _size: ", _size);
-
-        _coeffs = new T[_size];
+    /*
+    0D: (0, {}) - scalar
+    1D: (1, {4}) - vector
+    2D: (2, {4, 4}) - matrix
+    3D: (3, {4, 4, 4})
+    */
+    explicit Tensor() {
+        // scalar
+        rank = 0;
+        dims = nullptr;
+        coeffs = new T[1];
     }
 
-    // // Copy Assignment Operator (Scalar)
-    // // tensor[index] = number;
-    // Tensor<T> & operator = (const T & scalar);
-    // // Index Operator []
+    // --- scalar(value) ---
+    // Tensor sc = scalar(42.0);
+    template <Arithmetic U>
+    friend Tensor<U> scalar(U value);
+    // ------
 
-    inline T get(std::integral auto ... args){
-        if(isScalar()){
-            return _value;
-        }
-        int dims[] = {args...}; // arguments expansion
-        int length = sizeof...(args);
-        if(length != _rank){
-            throw std::runtime_error("Error: invalid indexes!");
-        }
-        return _coeffs[index(dims)];
+    explicit Tensor(int rank, int dims[]);
+
+    explicit Tensor(std::integral auto ... args); // C++20, abbreviated function templates with concept
+    explicit Tensor(auto ... args){
+        throw std::runtime_error("Incorrect type: only ints in explicit Tensor(int ... dims)");
     }
 
-    inline Tensor<T> & set(T value, std::integral auto ... args){
-        if(isScalar()){
-            _value = value;
-        }
-        else {
-            int dims[] = {args...}; // arguments expansion
-            int length = sizeof...(args);
-            if(length != _rank){
-                throw std::runtime_error("Error: invalid indexes!");
-            }
-            _coeffs[index(dims)] = value;
-        }
-        return *this;
-    }
+    // --- initializer_list ---
+    Tensor(const INITIALIZER_LIST_1(T) list);
+    Tensor(const INITIALIZER_LIST_2(T) list);
+    Tensor(const INITIALIZER_LIST_3(T) list);
+    Tensor(const INITIALIZER_LIST_4(T) list);
+    // ------
 
-    inline bool isScalar() const { return _rank == 0 && _size == -1 && _coeffs == nullptr && _shape == nullptr; }
-    inline bool isVector() const { return _rank == 1 && _size != -1 && _coeffs != nullptr && _shape != nullptr; }
-    inline bool isMatrix() const { return _rank == 2 && _size != -1 && _coeffs != nullptr && _shape != nullptr; }
 
-    inline int size() const { return _size; }
-    inline int rank() const { return _rank; }
-    inline int * shape() const { return _shape; }
+    // --- Rule of 5 ---
+    // Destructor
+    /*virtual*/ ~Tensor();
+    // don't know yet will there be inheritance from Tensor, probably it's okay to make destructor virtual
+    // virtual добавляет 1 указатель на vtable (8 byte), поэтому без virtual.
 
-    inline T& value(){
-        #if DEBUG_TENSOR
-            if(!isScalar()){
-                throw std::runtime_error("Error: Geting a value of a non-scalar tensor!");
-            }
-        #endif
-        return _value;
-    }
+    // Copy Constructor
+    Tensor(const Tensor<T> & other);
 
-    inline int index(const int * const dims) const {
-        int index = 0;
-        if(dims[0] >= _shape[0]){
-            throw std::runtime_error("Error: out of bounds!");
-        }
-        for(int i = 0; i < _rank - 1; i++){
-            index += _shape[i + 1] * dims[i];
-        }
-        index += dims[_rank - 1]; // last element
-        return index;
-    }
+    // Copy Assignment Operator
+    Tensor<T> & operator = (const Tensor<T> & other);
 
-    std::string toString(){
-        if(isScalar()){
-            return "tensor<" + std::string(typeid(T).name()) + ">: " + std::to_string(_value);
-        }
+    // Move Constructor
+    Tensor(Tensor<T> && other);
 
-        std::string result = "tensor<" + std::string(typeid(T).name()) + ">: \n";
+    // Move Assignment Operator
+    Tensor<T> & operator = (Tensor<T> && other); // tensor[index] = Tensor();
+    // ------
 
-        for(int i = 0; i < _size; i++){
-            result += std::string(" ") + std::to_string(_coeffs[i]);
-            if((i + 1) % _shape[_rank - 1] == 0){
-                result += "\n";
-            }
-            if(_rank > 2){
-                if((i + 1) % (_shape[_rank - 1] * _shape[_rank - 2]) == 0){
-                    result += "\n";
-                }
-            }
-        }
-        if(_rank > 2) result.pop_back(); // last '\n'
-        return result;
-    }
+    // --- Index Operator [] ---
+    // The idea is to call tensor[d][r][c], which would be nice.
+    // Here is the problem with proxy "TensorView" which should not make copy, or should idk.
+    // inline Tensor& operator [] (const int index) {
+    //     return coeffs[index];
+    // }
+    inline T& get(std::integral auto ... args);
+
+    // --- toString ---
+    std::string toString() const;
+
+    // Stream insertion operation <<
+    template <Arithmetic U>
+    friend std::ostream& operator<<(std::ostream& os, const Tensor<U>& tensor);
 
 };
 
-} // namespace tensor
 
-/*
-    Implementation of template class is in .tpp file
- */
+// --- scalar(value) ---
+// Tensor sc = scalar(42.0);
+template <Arithmetic U>
+/*friend*/ Tensor<U> scalar(U value){
+    Tensor<U> tensor;
+    *tensor.coeffs = value; // tensor[0]
+    return tensor; // RVO
+}
+// ------
+
+
+template <Arithmetic U>
+std::string Tensor<U>::toString() const {
+    // print undefined tensor
+    if(rank < 0 && dims == nullptr && coeffs == nullptr){
+        return "tensor(null)";
+    }
+    std::string res = "";
+    res += "tensor(" + std::to_string(rank) + "D)<" + std::string(typeid(coeffs[0]).name()) + ">:\n";
+    int size = 1; for(int i = 0; i < rank; i++) size *= dims[i];
+    for(int i = 0; i < size; i++){
+        if(rank >= 1 && i % dims[rank - 1] == 0 && i != 0){
+            res += "\n";
+        }
+        if(rank >= 2 && i % (dims[rank - 1] * dims[rank - 2]) == 0 && i != 0){
+            res += "\n";
+        }
+        res += " " + std::to_string(coeffs[i]);
+    }
+    return res;
+}
+
+// Stream insertion operation <<
+template <Arithmetic U>
+/*friend*/ std::ostream& operator << (std::ostream& os, const Tensor<U>& tensor){
+    os << tensor.toString();
+    return os;
+}
+
+template <Arithmetic U>
+Tensor<U>::Tensor(int rank, int dims[]){ // int dims[] is int * dims
+    if(rank < 0){
+        throw std::runtime_error("Error: Null tensor, rank is invalid!");
+    }
+    if(dims == nullptr){
+        throw std::runtime_error("Error: Null tensor, dims is invalid!");
+    }
+    this->rank = rank;
+    this->dims = new int[rank];
+    for(int i = 0; i < rank; i++){
+        this->dims[i] = dims[i];
+    }
+
+    int size = 1; for(int i = 0; i < rank; i++) size *= dims[i];
+    coeffs = new U[size];
+}
+
+// Constructor : Tensor tensor(depth, rows, cols)
+template <Arithmetic U>
+Tensor<U>::Tensor(std::integral auto ... args) { // C++20, abbreviated function templates with concept
+    int _dims[] = {args...}; // arguments expansion
+    // rank = sizeof(dims) == 0 ? 0 : sizeof(dims) / sizeof(dims[0]); // number of dim entries
+    rank = sizeof...(args); // number of dim entries
+    dims = new int[rank];
+    for(int i = 0; i < rank; i++){
+        dims[i] = _dims[i];
+    }
+    int size = 1; for(int i = 0; i < rank; i++) size *= dims[i];
+    coeffs = new U[size];
+}
+
+
+// --- initializer_list ---
+
+// Tensor vector = {1, 2, 3};
+template <Arithmetic T>
+Tensor<T>::Tensor(const INITIALIZER_LIST_1(T) list){
+    // 1D: {0, 1, 2}
+    rank = 1;
+    int size = list.size();
+    dims = new int[rank]{size};
+    coeffs = new T[size];
+    int i = 0;
+    for(auto e: list){
+        // we can't access initializer_list by index, only by range-based for loop or by iterator
+        coeffs[i++] = e;
+    }
+}
+
+// Tensor matrix = {{1, 2}, {3, 4}};
+template <Arithmetic T>
+Tensor<T>::Tensor(const INITIALIZER_LIST_2(T) list){
+    // 2D:
+    // {{0, 1, 2},
+    //  {3, 4, 5},
+    //  {6, 7, 8}}
+    rank = 2;
+    int rows = list.size(),
+        cols = list.begin()->size();
+    dims = new int[rank]{rows, cols};
+    int size = rows * cols;
+    coeffs = new T[size];
+    // we can't access initializer_list by index, only by range-based for loop or by iterator
+    int i = 0;
+    for(auto & r: list){
+        for(auto c: r){
+            // std::cout << c << std::endl;
+            coeffs[i++] = c;
+        }
+    }
+}
+
+template <Arithmetic T>
+Tensor<T>::Tensor(const INITIALIZER_LIST_3(T) list){
+    rank = 3;
+    int depth = list.size(),
+        rows = list.begin()->size(),
+        cols = list.begin()->begin()->size();
+    dims = new int[rank]{depth, rows, cols};
+    int size = depth * rows * cols;
+    coeffs = new T[size];
+    // we can't access initializer_list by index, only by range-based for loop or by iterator
+    int i = 0;
+    for(auto & d: list){
+        for(auto & r: d){
+            for(auto c: r){
+                // std::cout << c << std::endl;
+                coeffs[i++] = c;
+            }
+        }
+    }
+}
+
+template <Arithmetic T>
+Tensor<T>::Tensor(const INITIALIZER_LIST_4(T) list){
+    rank = 4;
+    int batch = list.size(),
+        depth = list.begin()->size(),
+        rows = list.begin()->begin()->size(),
+        cols = list.begin()->begin()->begin()->size();
+    dims = new int[rank]{batch, depth, rows, cols};
+    int size = batch * depth * rows * cols;
+    coeffs = new T[size];
+    // we can't access initializer_list by index, only by range-based for loop or by iterator
+    int i = 0;
+    for(auto & b : list){
+        for(auto & d: b){
+            for(auto & r: d){
+                for(auto c: r){
+                    // std::cout << c << std::endl;
+                    coeffs[i++] = c;
+                }
+            }
+        }
+    }
+}
+
+// ------
+
+
+// --- Rule of 5 ---
+
+template <Arithmetic T>
+Tensor<T>::~Tensor(){
+    std::cout << "Destructor" << std::endl;
+    rank = -1;
+    if(coeffs != nullptr){
+        delete[] coeffs;
+        coeffs = nullptr;
+    }
+    if(dims != nullptr){
+        delete[] dims;
+        dims = nullptr;
+    }
+}
+
+
+// Copy Constructor
+// O(N)
+template <Arithmetic T>
+Tensor<T>::Tensor(const Tensor<T> & other) {
+    std::cout << "Copy Constructor" << std::endl;
+
+    /*
+    No need in delete[] coeffs and dims, because it is a constructor
+    */
+
+    rank = other.rank;
+
+    int size = 1;
+
+    if(rank > 0 && other.dims != nullptr){
+        dims = new int[rank];
+        for(int i = 0; i < rank; i++){
+            dims[i] = other.dims[i];
+            size *= other.dims[i];
+        }
+    }
+
+    coeffs = new T[size];
+
+    for(int i = 0; i < size; i++){
+        coeffs[i] = other.coeffs[i];
+    }
+}
+
+
+// Copy Assignment Operator
+// O(N)
+template <Arithmetic T>
+Tensor<T> & Tensor<T>::operator = (const Tensor<T> & other){
+    std::cout << "Copy Assignment Operator" << std::endl;
+    if(this != &other){
+        /*
+        // Copy assignment should not require rank and dims match,
+        // users should check it themself if needed.
+
+        if (rank != other.rank){
+            throw std::runtime_error("Error: rank doesn't match for copy assignment");
+        }
+        if(dims[i] != other.dims[i]){
+            throw std::runtime_error("Error: dims doesn't match for copy assignment");
+        }
+        */
+
+        rank = -1;
+        if(coeffs != nullptr){
+            delete[] coeffs;
+            coeffs = nullptr;
+        }
+        if(dims != nullptr){
+            delete[] dims;
+            dims = nullptr;
+        }
+
+        rank = other.rank;
+
+        int size = 1;
+        if(rank > 0 && other.dims != nullptr){
+            dims = new int[rank];
+            for(int i = 0; i < rank; i++){
+                dims[i] = other.dims[i];
+                size *= other.dims[i];
+            }
+        }
+
+        coeffs = new T[size];
+        for(int i = 0; i < size; i++){
+            coeffs[i] = other.coeffs[i];
+        }
+    }
+    return *this;
+}
+
+
+// Move Constructor
+// O(1)
+template <Arithmetic T>
+Tensor<T>::Tensor(Tensor<T> && other){
+    std::cout << "Move Constructor" << std::endl;
+    // this = other
+    rank = other.rank;
+    dims = other.dims;
+    coeffs = other.coeffs;
+    // other = null, reset but not delete
+    other.rank = -1;
+    other.dims = nullptr;
+    other.coeffs = nullptr;
+}
+
+
+// Move Assignment Operator
+// O(1)
+template <Arithmetic T>
+Tensor<T> & Tensor<T>::operator = (Tensor<T> && other){
+    std::cout << "Move Assignment Operator" << std::endl;
+    if(this != &other){
+        /*
+        // Copy assignment should not require rank and dims match,
+        // users should check it themself if needed.
+        if (rank != other.rank){
+            throw std::runtime_error("Error: rank doesn't match for move assignment");
+        }
+        for(int i = 0; i < rank; i++){
+            if(dims[i] != other.dims[i]){
+                throw std::runtime_error("Error: dims doesn't match for move assignment");
+            }
+        }
+        */
+        rank = -1;
+        if(coeffs != nullptr){
+            delete[] coeffs;
+            coeffs = nullptr;
+        }
+        if(dims != nullptr){
+            delete[] dims;
+            dims = nullptr;
+        }
+
+        // this = other
+        rank = other.rank;
+        dims = other.dims;
+        coeffs = other.coeffs;
+        // other = null, reset but not delete
+        other.rank = -1;
+        other.dims = nullptr;
+        other.coeffs = nullptr;
+
+    }
+    return *this;
+}
+
+// ------
+
+
+template <Arithmetic U>
+inline U& Tensor<U>::get(std::integral auto ... args){
+    int _dims[] = {args...}; // arguments expansion (not compute free operation)
+    if(rank != sizeof...(args)){ // number of dim entries
+        throw std::runtime_error("Error: incorrect number of indexes");
+    }
+    int index = 0; // mapping
+    int stride = 1;
+    for(int d = rank - 1; d >= 0; --d){
+        if(d != rank - 1){
+            stride *= dims[d + 1];
+        }
+        index += _dims[d] * stride;
+    }
+    return coeffs[index];
+}
+
+} // namespace tensor
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,128 @@
+/*
+    SPDX-License-Identifier: MPL-2.0
+    --------------------------------
+    This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+    If a copy of the MPL was not distributed with this file,
+    You can obtain one at https://mozilla.org/MPL/2.0/.
+
+    This file is part of the Tensor-library:
+    https://github.com/alarxx/Tensor-library
+
+    Provided “as is”, without warranty of any kind.
+
+    Copyright © 2026 Alar Akilbekov. All rights reserved.
+ */
+
 #include <iostream>
+#include <cassert>
+#include <stdexcept>
+#include <string>
+#include <type_traits>
+#include <concepts>
+#include <initializer_list>
+#include <vector>
 
 #include "Tensor/Tensor.hpp"
 
-using tensor::Tensor;
+// using namespace tensor;
+using   tensor::Tensor,
+        tensor::scalar;
 
 int main(){
-    std::cout << "--- Tensor.cpp execution started! ---" << std::endl;
 
-    std::cout << "sizeof(Tensor): " << sizeof(Tensor<double>) << std::endl;
+    Tensor sc1 = scalar(42.0);
+    Tensor sc2 = scalar(45.0);
+    sc1.get() = sc2.get();
+    sc1.get() += 1;
+    std::cout << "scalar: " << sc1.get() << std::endl; // 46
+    std::cout << "scalar: " << sc2.get() << std::endl; // 45
 
-    Tensor scalar;
-    scalar.set(45.);
-    std::cout << scalar.get() << std::endl;
-    std::cout << scalar.toString() << std::endl;
+    int dims[]{3, 3, 3};
+    Tensor<float> t(3, dims);
 
-    Tensor tensor(3, 3, 3);
+    /*
+    Tensor t(3, 3, 3);
+    std::cout << t << std::endl;
+    */
 
-    tensor.set(42., 0, 0, 1);
-    std::cout << tensor.get(0, 0, 1) << std::endl;
+    /*
+    t.get(1, 1, 1) = 3;
+    std::cout << "t.get(1, 1, 1): " << t.get(1, 1, 1) << std::endl;
+    */
 
-    std::cout << tensor.toString();
+    /*
+    // --- initializer_list ---
+    Tensor vec = {1, 2, 3};
+    std::cout << "vec.get(1): " << vec.get(1) << std::endl;
 
-    std::cout << "--- Tensor.cpp execution ended! ---" << std::endl;
+    Tensor mat = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9},
+    };
+    std::cout << "mat.get(1, 1): " << mat.get(1, 1) << std::endl;
+
+    Tensor d3 = {
+        {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        },
+        {
+            {10, 11, 12},
+            {10, 11, 12},
+            {10, 11, 12}
+        },
+    };
+
+    Tensor d4 = { // 4D
+        {{ // 3D
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        },
+        {
+            {10, 11, 12},
+            {10, 11, 12},
+            {10, 11, 12}
+        }},
+        {{ // 3D
+            {13, 14, 15},
+            {16, 17, 18},
+            {19, 20, 21},
+        },
+        {
+            {22, 23, 24},
+            {25, 26, 27},
+            {28, 29, 30}
+        }},
+    };
+    std::cout << "d4: " << d4 << std::endl;
+    // ------
+    */
+
+    /*
+    // #include <vector>
+    std::vector<int> stdvec {1, 2, 3};
+    std::vector<int> stdvec2 {};
+    std::cout << stdvec.size() << std::endl; // 3
+    std::cout << stdvec2.size() << std::endl; // 0
+    stdvec2 = stdvec;
+    std::cout << stdvec2.size() << std::endl; // 3
+
+    Tensor<float> vec = {1, 2, 3};
+    // Tensor t2 = vec;
+    // Tensor t2 = std::move(vec);
+    Tensor<float> t2;
+    t2 = vec;
+    // t2 = std::move(vec);
+    t2.get(0) = 5;
+    t2.get(1) = 6;
+    t2.get(2) = 7;
+    std::cout << "vec " << vec << std::endl;
+    std::cout << "t2 " << t2 << std::endl;
+    */
+
 }
 
 /*
